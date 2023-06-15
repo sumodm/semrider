f69302bfa5bb5181033345643d3983f48dbd3b84
Sachin <sachin@Sachins-MacBook-Air.local>
2023-06-15 13:20:45 +0530

    Update and Search Index added.

--- server/README.md
+++ server/README.md
@@ -1,1 +1,2 @@
-All server code goes here
+Please download encoder model from https://drive.google.com/file/d/1JLTYMaCtY4pkl4oeygXVnk_GxJpOWxKH/view?usp=drive_link and put it in the server folder.
+```
---
+++ server/server.py
@@ -0,0 +1,95 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+from transformers import AutoTokenizer
+import onnxruntime
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify
+
+
+chunk_size = 200
+
+model_ckpt = "sentence-transformers/multi-qa-mpnet-base-dot-v1"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+session = onnxruntime.InferenceSession("traced_bert.onnx")
+nltk.download('punkt')
+app = Flask(__name__)
+
+
+def load_files():
+    try:
+        embeds = pickle.load(open("embeds.pkl", "rb"))
+        embeds = np.array(embeds).reshape(len(embeds), -1)
+    except FileNotFoundError:
+        embeds = np.array([])
+    try:
+        sites = pickle.load(open("sites.pkl", "rb"))
+    except FileNotFoundError:
+        sites = []
+    try:
+        texts = pickle.load(open("texts.pkl", "rb"))
+    except FileNotFoundError:
+        texts = []
+    return embeds, sites, texts
+
+
+def dump_files(embeds, sites, texts):
+    pickle.dump(embeds, open("embeds.pkl", "wb"))
+    pickle.dump(sites, open("sites.pkl", "wb"))
+    pickle.dump(texts, open("texts.pkl", "wb"))
+
+
+def get_embeddings(text):
+    embedded_text = tokenizer(text, padding="max_length", truncation=True)
+    ort_inputs = {"input_ids": np.array(embedded_text["input_ids"]).reshape(1,-1),
+                  "attention_mask" : np.array(embedded_text["attention_mask"]).reshape(1,-1)}
+    ort_outputs = session.run(None, ort_inputs)
+    return ort_outputs[0]
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    received_text = request.json.get('text')
+    site = request.json.get('site')
+    embeds, sites, texts = load_files()
+    sentences = sent_tokenize(received_text)
+    idx = 0
+    text = ""
+    for sentence in sentences:
+        idx = idx + len(tokenizer.tokenize(sentence))
+        text += sentence
+        if idx >= chunk_size:
+            sites.append(site)
+            texts.append(text)
+            if embeds.size == 0:
+                embeds = get_embeddings(text)
+            else:
+                embeds = np.vstack([embeds, get_embeddings(text)])
+            text = ""
+            idx = 0
+    else:
+        sites.append(site)
+        texts.append(text)
+        if embeds.size == 0:
+            embeds = get_embeddings(text)
+        else:
+            embeds = np.vstack([embeds, get_embeddings(text)])
+    dump_files(embeds, sites, texts)
+    return jsonify({'success': True})
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    embeds, sites, texts = load_files()
+    question = request.json.get('question')
+    number_of_results = int(request.json.get('number_of_results'))
+    question_embed = get_embeddings(question)
+    similarities = embeds.dot(question_embed.T)
+    top_indices = np.argsort(similarities.flatten())[-number_of_results:][::-1]
+    top_context = [texts[i] for i in top_indices]
+    top_sites = [sites[i] for i in top_indices]
+    return jsonify({'top_sites': top_sites, 'top_context': top_context})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
